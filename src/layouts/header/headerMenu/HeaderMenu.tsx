71d927507ce353a0f2abab5643697a68c3fd40d5
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-02 22:29:51 +0300

    feat: create HeaderMenu component

---
+++ src/layouts/header/headerMenu/HeaderMenu.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/anchor-has-content */
+import React from 'react';
+import styled from 'styled-components';
+
+type ListItemsPropsType = {
+    menuItems: Array<string>
+}
+
+export const HeaderMenu = (props: ListItemsPropsType) => {
+    return (
+        <StyledHeaderMenu>
+            <List>
+                {props.menuItems.map((item, index) => {
+                return <ListItem key={index}>
+                        <Link href='#'>{item}</Link>
+                    </ListItem>
+                })}
+            </List>
+        </StyledHeaderMenu>
+    );
+};
+
+const StyledHeaderMenu = styled.nav``;
+
+const List = styled.ul`
+    display: flex;
+    gap: 80px;
+`;
+
+const ListItem = styled.li``;
+
+const Link = styled.a``;
