3060897b3850fdc5ecb529bc6b67e8bd057a10c5
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-02 21:17:23 +0300

    feat: create Theme component

---
+++ src/styles/Theme.tsx
@@ -0,0 +1,9 @@
+export const Theme = {
+    color: {
+        mainBG: '#0f1624',
+        secondBG: '#0f1624',
+        gradient: 'linear-gradient(270deg, #13adc7 0%, #6978d1 66.67%, #945dd6 100%);',
+        border: '#a39d9d',
+        font: '#fff'
+    }
+}
