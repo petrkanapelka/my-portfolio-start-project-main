96a4f56e90da3b192dbe4774aad98aa9b6ba2f5e
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-03 00:07:27 +0300

    feat: add Link component

---
+++ src/components/link/Link.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Theme } from '../../styles/Theme';
+
+type LinkPropsType = {
+    content: string,
+    url: string,
+}
+
+export const Link = (props: LinkPropsType) => {
+    return (
+        <StyledLink href={props.url}>{props.content}</StyledLink>
+    );
+};
+
+const StyledLink = styled.a`
+    background: ${Theme.color.gradient};
+    padding: 15px 66px;
+    font-weight: 600;
+    font-size: 20px;
+    border-radius: 83px;
+
+`
