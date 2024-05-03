06eab10ff0402afc6aae1c1cbf246c9a1099e3e6
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-04 00:10:57 +0300

    style: delete background

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
         transition: all 0.2s ease;
-        background-color: ${Theme.color.mainBG};
+        // background-color: ${Theme.color.mainBG};
     }
     body {
         margin: 0;
