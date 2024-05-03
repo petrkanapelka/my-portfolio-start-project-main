2e7b9542acc712480d3d3be56efca023c9c2a93a
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-03 22:17:19 +0300

    fix: fix secondBG color

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -1,7 +1,7 @@
 export const Theme = {
     color: {
         mainBG: '#0f1624',
-        secondBG: '#0f1624',
+        secondBG: '#161d2a',
         gradient: 'linear-gradient(270deg, #13adc7 0%, #6978d1 66.67%, #945dd6 100%);',
         border: '#a39d9d',
         font: '#fff'
