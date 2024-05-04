3d4b0fff78da63484001fff4c7e1d93db1ed03f4
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-04 09:47:54 +0300

    feat:  add to App Experience component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Header } from './layouts/header/Header';
 import { Main } from './layouts/main/Main';
+import { Experience } from './layouts/sections/experience/Experience';
 import { Projects } from './layouts/sections/projects/Projects';
 import { Technologies } from './layouts/sections/technologies/Technologies';
 
@@ -8,10 +9,11 @@ import { Technologies } from './layouts/sections/technologies/Technologies';
 function App() {
     return (
         <div className="App">
-            <Header/>
-            <Main/>
-            <Projects/>
-            <Technologies/>
+            <Header />
+            <Main />
+            <Projects />
+            <Technologies />
+            <Experience />
         </div>
     );
 }
