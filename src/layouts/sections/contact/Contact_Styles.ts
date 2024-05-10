5cd09e116d4e6a91233409229397b0ffa56cc5f8
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-10 22:48:43 +0300

    feat: add styles for Contact component

---
+++ src/layouts/sections/contact/Contact_Styles.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import { Theme } from "../../../styles/Theme";
+
+const Contact = styled.section`
+    padding: 100px 0 140px;
+`;
+
+const Form = styled.form`
+  max-width: 540px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  margin: 0 auto;
+
+  textarea {
+    resize: none;
+    height: 155px;
+  }
+`;
+
+const Field = styled.input`
+  width: 100%;
+  border: 1px solid ${Theme.color.border};
+  background-color: ${Theme.color.secondBG};
+  padding: 7px 15px;
+  font-weight: 400;
+  font-size: 12px;
+  letter-spacing: 0.05em;
+  color: ${Theme.color.font};
+  &::placeholder {
+    color: ${Theme.color.placeholderColor};
+    text-transform: capitalize;
+  }
+
+  &:focus-visible {
+    outline: 1px solid ${Theme.color.border};
+  }
+`;
+
+const SubmitButton = styled.button`
+    background: ${Theme.color.gradient};
+    padding: 15px 66px;
+    font-weight: 600;
+    font-size: 20px;
+    border-radius: 83px;
+`
+
+
+export const S = {
+  Contact,
+  Field,
+  Form,
+  SubmitButton
+};
