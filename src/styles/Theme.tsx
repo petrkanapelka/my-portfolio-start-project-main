767bbea75c5b77a809b046f36a115386157f7ccb
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-11 01:34:58 +0300

    feat: add hover colors for Theme

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -3,8 +3,11 @@ export const Theme = {
         mainBG: '#0f1624',
         secondBG: '#161d2a',
         gradient: 'linear-gradient(270deg, #13adc7 0%, #6978d1 66.67%, #945dd6 100%);',
+        gradientReversed: 'linear-gradient(270deg, #945dd6 0%, #6978d1 66.67%, #13adc7 100%);',
         border: '#a39d9d',
         font: '#fff',
         placeholderColor:  '#495057',
+        firstHoverColor: "#13adc7",
+        secondHoverColor: "#945dd6",
     }
 }
