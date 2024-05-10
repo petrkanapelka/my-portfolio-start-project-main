5029fa2977d9f0a53967fec20872f10542f4de71
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-11 01:35:31 +0300

    style: add hover styles for SubmitButton

--- src/layouts/sections/contact/Contact_Styles.ts
+++ src/layouts/sections/contact/Contact_Styles.ts
@@ -40,12 +40,34 @@ const Field = styled.input`
 `;
 
 const SubmitButton = styled.button`
+    position: relative;
     background: ${Theme.color.gradient};
     padding: 15px 66px;
     font-weight: 600;
     font-size: 20px;
     border-radius: 83px;
-`
+    z-index: 9999;
+    overflow: hidden;
+
+    &:before {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 0%;
+        height: 100%;
+        background-color: ${Theme.color.firstHoverColor};
+        border-radius: 83px;
+        z-index: -99999;
+    }
+
+    &:hover {
+        &:before {
+            width: 100%;
+        }
+    }
+`;
+
 
 
 export const S = {
