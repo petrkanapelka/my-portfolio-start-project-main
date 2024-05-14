a6506cdcb785ff1c7e9a5ca7f6a5a571774d357a
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-15 00:09:35 +0300

    refactor: Logo component now uses styled-components for styling

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,33 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Icon } from '../icon/Icon';
-import { Theme } from '../../styles/Theme';
+import { S } from './Logo_Styles';
 
-export const Logo = () => {
+export const Logo: React.FC = () => {
     return (
-        <StyledLogo href='#'>
+        <S.Logo href='#'>
             <Icon iconId='logo' width='47px' height='47px' viewbox='0 0 47px 47px' />
-            <LogoTitle>Portfolio</LogoTitle>
-        </StyledLogo>
+            <S.LogoTitle>Portfolio</S.LogoTitle>
+        </S.Logo>
     );
 };
-
-const StyledLogo = styled.a`
-    display: flex;
-    align-items: center;
-    flex-grow:1;
-    gap: 12px;
-
-    &:hover {
-        color: #13adc7;
-    }
-
-    @media ${Theme.media.largeTablet} {
-        align-self: flex-start;
-    }
-`;
-
-const LogoTitle = styled.span`
-    font-weight: 500;
-    font-size: 30px;
-`
