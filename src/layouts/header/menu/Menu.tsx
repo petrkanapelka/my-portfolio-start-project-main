4e3941621549ad6d7d599f196760eb98bd5f669e
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-14 23:23:19 +0300

    refactor: move styled components to a separate file Menu_Styles and convert Menu component to TypeScript

--- src/layouts/header/menu/Menu.tsx
+++ src/layouts/header/menu/Menu.tsx
@@ -1,45 +1,17 @@
-import styled from 'styled-components';
-import { Theme } from '../../../styles/Theme';
-import { SocialMenu } from '../../../components/socialMenu/SocialMenu';
-import { socialItems } from '../Header';
+import { SocialMenu, socialItems } from '../../../components/socialMenu/SocialMenu';
 import { NavigationMenu } from './navigationMenu/NavigationMenu';
+import { S } from './Menu_Styles';
 
 const navItems = ['Home', 'Projects', 'Technologies', 'About me', 'Contact me'];
 
 
-export const Menu = (props: {isOpen: Boolean}) => {
+export const Menu: React.FC<{isOpen: Boolean}> = (props: {isOpen: Boolean}) => {
     return (
-            <StyledMenu isOpen={props.isOpen}>
+            <S.Menu isOpen={props.isOpen}>
                 <NavigationMenu menuItems={navItems} />
                 <SocialMenu iconsId={socialItems.inconsID}
                     socialLinks={socialItems.socialLinks}
                     width='32px' height='32px' viewbox='0 0 32 32' />
-            </StyledMenu>
+            </S.Menu>
     );
 };
-
-const StyledMenu = styled.div<{ isOpen: Boolean }>`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-grow:1;
-    position: relative;
-    top: 0px;
-    transform: scaleY(1);
-    transform-origin: top center;
-    opacity: 1;
-
-    @media ${Theme.media.largeTablet} {
-        width: 100%;
-    }
-
-    @media ${Theme.media.tablet} {
-        position: ${({ isOpen }) => isOpen ? 'relative' : 'absolute'};
-        top: ${({ isOpen }) => isOpen ? '0px' : '73px'};
-        transform: scaleY(${({ isOpen }) => isOpen ? 1 : 0});
-        opacity: ${({ isOpen }) => isOpen ? 1 : 0};
-        flex-direction: column;
-        gap: 20px;
-        transition: transform 0.3s ease, opacity 0.3s ease;
-    }
-    `
