099f5838d735358fff068540ff70abf1d6c5a44d
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-14 23:23:58 +0300

    refactor: move styled components to Menu_Styles and convert BurgerMenu component to TypeScript

--- src/layouts/header/menu/burgerMenu/BurgerMenu.tsx
+++ src/layouts/header/menu/burgerMenu/BurgerMenu.tsx
@@ -1,50 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Theme } from '../../../../styles/Theme';
+import { S } from '../Menu_Styles';
 
-/* type BurgerMenuPropsType = {
-    isOpen: boolean;
-}; */
-
-
-export const BurgerMenu = (props: {isOpen: Boolean}) => {
+export const BurgerMenu: React.FC<{ isOpen: Boolean }> = (props: { isOpen: Boolean }) => {
     const openClass = props.isOpen ? 'open' : '';
 
     return (
-        <StyledBurgerMenu isOpen={props.isOpen}>
+        <S.BurgerMenu isOpen={props.isOpen}>
             <span className={openClass}></span>
             <span className={openClass}></span>
             <span className={openClass}></span>
-        </StyledBurgerMenu>
+        </S.BurgerMenu>
     );
 };
-
-const StyledBurgerMenu = styled.div<{ isOpen: Boolean }>`
-    position: absolute;
-    right: 15px;
-    top: 15px;
-    display: none;
-    span {
-        display: block;
-        width: 36px;
-        height: 2px;
-        background-color: ${Theme.color.font};
-        margin: 5px 0;
-
-        &:nth-child(1) {
-            transform: ${({ isOpen }) => isOpen ? 'translate(0, 7px) rotate(45deg)' : 'none'};
-        }
-
-        &:nth-child(2) {
-            opacity: ${({ isOpen }) => isOpen ? 0 : 1};
-        }
-
-        &:nth-child(3) {
-            transform: ${({ isOpen }) => isOpen ? 'translate(0, -7px) rotate(-45deg)' : 'none'};
-        }
-    }
-
-    @media ${Theme.media.tablet} {
-        display: block;
-    }
-`;
