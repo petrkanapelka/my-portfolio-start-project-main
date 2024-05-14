e26e72cdce5330d221e03da58a89d4b3b4a5f0f8
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-14 09:20:05 +0300

    feat: enhance footer responsiveness and styling

--- src/layouts/footer/Footer.tsx
+++ src/layouts/footer/Footer.tsx
@@ -12,8 +12,8 @@ export const Footer = () => {
             <Container>
                 <FlexWrapper justify='space-between' alignItems='center'>
                     <FooterLinks>
-                        <FooterLink href="tel:+1234567890">Call me:<br />123-456-789</FooterLink>
-                        <FooterLink href="mailto:example@example.com">Email:<br />xxx@xyz.com</FooterLink>
+                        <FooterLink href="tel:+1234567890"><span>Call me:</span><br />123-456-789</FooterLink>
+                        <FooterLink href="mailto:example@example.com"><span>Email:</span><br />xxx@xyz.com</FooterLink>
                     </FooterLinks>
                     <SocialMenu iconsId={socialItems.inconsID} socialLinks={socialItems.socialLinks} />
                 </FlexWrapper>
@@ -24,20 +24,49 @@ export const Footer = () => {
 
 const StyledFooter = styled.section`
     padding: 42px 0;
+
+    @media ${Theme.media.tablet} {
+        padding: 50px 0;
+    }
+
+    @media ${Theme.media.mobile} {
+        ${FlexWrapper} {
+            flex-direction: column;
+            gap:20px;
+        }
+    }
 `
 const FooterLinks = styled.div`
     display: flex;
-    gap: 162px;
+    justify-content: space-between;
+    align-items: center;
+    width: 50%;
+
+    @media ${Theme.media.mobile} {
+        flex-direction: column;
+        width: 100%;
+        gap: 10px;
+    }
 `
 
 const FooterLink = styled.a`
     display: inline-block;
     font-weight: 600;
-    font-size: 22px;
+    font-size: calc( (100vw - 360px)/(1440 - 360) * (22 - 16) + 16px);
     position: relative;
 
     &:hover {
         color: ${Theme.color.firstHoverColor};
         transform: translateY(-4px);
+    }
+
+    @media ${Theme.media.mobile} {
+        br {
+            display: none;
+        }
+
+        span {
+            margin-right: 5px;
+        }
     }
 `
