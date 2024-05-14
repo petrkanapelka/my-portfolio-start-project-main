660dcc241d70390a46214bb8dce46f8196c5c368
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-14 23:56:30 +0300

    refactor: Converted the ExpYear component to TypeScript with React.FC type and imported styled components from Experience_Styles.

--- src/layouts/sections/experience/expYear/ExpYear.tsx
+++ src/layouts/sections/experience/expYear/ExpYear.tsx
@@ -1,71 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Theme } from '../../../../styles/Theme';
+import { S } from '../Experience_Styles';
 
 type ExpYearPropsType = {
     year: number;
     descr: string;
+    expYearRef: React.RefObject<HTMLDivElement>;
 }
 
-export const ExpYear = ({ year, descr, expYearRef }: ExpYearPropsType & { expYearRef: React.RefObject<HTMLDivElement> }) => {
+export const ExpYear: React.FC<ExpYearPropsType> = ({ year, descr, expYearRef }) => {
     return (
-        <StyledExpYear ref={expYearRef}>
-            <Year>{year}</Year>
-            <Description>{descr}</Description>
-        </StyledExpYear>
+        <S.ExpYear ref={expYearRef}>
+            <S.Year>{year}</S.Year>
+            <S.Description>{descr}</S.Description>
+        </S.ExpYear>
     );
 };
 
-const StyledExpYear = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-
-    @media ${Theme.media.tablet} {
-        display: none;
-
-        &:nth-child(1) {
-            display: flex;
-        }
-
-        &:nth-child(2) {
-            display: flex;
-        }
-    }
-
-    @media ${Theme.media.mobile} {
-        display: none;
-
-        &:nth-child(2) {
-            display: none;
-        }
-    }
-`
-
-const Year = styled.h3`
-    font-weight: 600;
-    font-size: 26px;
-    position: relative;
-    margin-bottom: 45px;
-    &::before {
-        content: '';
-        border-radius: 100%;
-        background-color: #fff;
-        width: 25px;
-        height: 25px;
-
-        position: absolute;
-        top: 100%;
-        left: 50%;
-        transform: translateX(-50%);
-    }
-`
-
-const Description = styled.p`
-    font-weight: 500;
-    font-size: 18px;
-    text-align: center;
-    padding: 0 5px;
-`
