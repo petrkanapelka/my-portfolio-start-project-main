6f4a55cb4e376a5cd9e0b472db1d570413ba9ec3
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-14 23:46:19 +0300

    Refactor: Converted the Technology component to TypeScript with React.FC type and imported styled components from Technologies_Styles

--- src/layouts/sections/technologies/technology/Technology.tsx
+++ src/layouts/sections/technologies/technology/Technology.tsx
@@ -1,50 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
+import { S } from '../Technologies_Styles';
 
 type TechnologyPropsType = {
     techName: string;
     techSkill: number;
 }
 
-export const Technology = (props: TechnologyPropsType) => {
+export const Technology:React.FC<TechnologyPropsType> = (props: TechnologyPropsType) => {
     return (
-        <StyledTechnology>
-            <TechName>{props.techName}</TechName>
-            <TechSkill techSkillScope={props.techSkill}></TechSkill>
-        </StyledTechnology>
+        <S.Technology>
+            <S.TechName>{props.techName}</S.TechName>
+            <S.TechSkill techSkillScope={props.techSkill}></S.TechSkill>
+        </S.Technology>
     );
 };
-
-const StyledTechnology = styled.div`
-    margin-bottom: 25px;
-    max-width: 900px;
-    width: 100%;
-`
-
-const TechName = styled.h3`
-    font-weight: 600;
-    font-size: 24px;
-    margin: 0 0 5px 15px;
-`
-
-type TechSkilPropsType = {
-    techSkillScope: number,
-}
-
-const TechSkill = styled.div<TechSkilPropsType>`
-    border-radius: 83px;
-    max-width: 900px;
-    width: 100%;
-    height: 18px;
-    background-color: #162950;
-    position: relative;
-
-    &::before {
-        content: '';
-        position: absolute;
-        width: ${props => props.techSkillScope}%;
-        height: 100%;
-        border-radius: 83px;
-        background: linear-gradient(270deg, #13adc7 0%, #6978d1 66.67%, #945dd6 100%);
-    }
-`
