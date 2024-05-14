42a55c0c0266e3e4187af301115a48d8dced2003
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-15 00:08:06 +0300

    refactor: Icon component now uses React.FC type for props.

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import iconSprite from '../../assets/icons/icon-sprite.svg'
 
 type IconPropsType = {
@@ -7,7 +8,7 @@ type IconPropsType = {
     viewbox?: string,
 }
 
-export const Icon = (props: IconPropsType) => {
+export const Icon: React.FC<IconPropsType> = (props: IconPropsType) => {
     return (
         <svg width={props.width || "35"} height={props.height || "35"} viewBox={props.viewbox || "0 0 35 35"} fill="none" xmlns="http://www.w3.org/2000/svg">
             <use xlinkHref={`${iconSprite}#${props.iconId}`}></use>
