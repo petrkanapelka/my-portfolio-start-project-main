dc3c8b2e648e53a10bd3537fa311fd612f2ed0ba
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-14 23:37:09 +0300

    refactor: move styled components to Project_Styles and convert Projects component to TypeScript

--- src/layouts/sections/projects/Projects.tsx
+++ src/layouts/sections/projects/Projects.tsx
@@ -1,38 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Container } from '../../../components/Container';
 import { FlexWrapper } from '../../../components/Flexwrapper';
 import { ProjectData } from './ProjectsData';
 import { Project } from './project/Project';
-import { Theme } from '../../../styles/Theme';
+import { S } from './Projects_Styles';
 
-
-
-
-export const Projects = () => {
+export const Projects: React.FC = () => {
     return (
-        <StyledProjects>
+        <S.Projects>
             <Container>
                 <SectionTitle>Projects</SectionTitle>
                 <FlexWrapper flexWrap='wrap' justify='space-around' alignItems='center'>
                     {ProjectData.map((prop, index) => {
-                        return <Project key={index} {...prop}/>
+                        return <Project key={index} {...prop} />
                     })}
                 </FlexWrapper>
             </Container>
-        </StyledProjects>
+        </S.Projects>
     );
 };
-
-const StyledProjects = styled.section`
-    padding: 100px 0 80px;
-
-    ${FlexWrapper} {
-        gap:10px;
-    }
-
-    @media ${Theme.media.tablet} {
-        padding: 50px 0;
-    }
-`
