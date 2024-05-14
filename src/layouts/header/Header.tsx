5fa230ce41167b2a86edc40714293191cf8ee3cc
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-14 23:21:50 +0300

    refactor: move styled components to a separate file Header_Styles and convert Header component to TypeScript

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,46 +1,21 @@
-import styled from 'styled-components';
 import { Logo } from '../../components/logo/Logo';
 import { Container } from '../../components/Container';
-import { Theme } from '../../styles/Theme';
 import { FlexWrapper } from '../../components/Flexwrapper';
 import { Menu } from './menu/Menu';
 import { BurgerMenu } from './menu/burgerMenu/BurgerMenu';
+import { S } from './Header_Styles';
+import React from 'react';
 
-
-export const socialItems = {
-    inconsID: ['github', 'linkedin', 'telegram'],
-    socialLinks: ['https://github.com/petrkanapelka', 'https://www.linkedin.com/', 'https://telegram.org/desktop']
-}
-
-export const Header = () => {
+export const Header: React.FC = () => {
     return (
-        <StyledHeader>
+        <S.Header>
             <Container>
                 <FlexWrapper justify='space-between' alignItems='center'>
                     <Logo />
                     <Menu isOpen={false} />
-                    <BurgerMenu isOpen={false}  />
+                    <BurgerMenu isOpen={false} />
                 </FlexWrapper>
             </Container>
-        </StyledHeader>
+        </S.Header>
     );
 };
-
-const StyledHeader = styled.header`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 34px 0;
-    background-color: ${Theme.color.secondBG};
-
-    @media ${Theme.media.largeTablet} {
-        ${FlexWrapper} {
-            flex-direction: column;
-        }
-    }
-
-    @media ${Theme.media.tablet} {
-        position: relative;
-        padding: 10px 0;
-    }
-`;
