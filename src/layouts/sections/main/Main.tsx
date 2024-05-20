a5a25fc523dfc04742d85c8c80347d3e91ba6875
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-20 23:11:38 +0300

    feat(main): enhance main section with typewriter effect and parallax tilt
    - Added Typewriter effect to the main slogan to cycle through different titles.
    - Implemented parallax tilt effect on the photo for a more dynamic visual.
    - Updated "Let’s Begin" link to a button that scrolls to the contact section smoothly.
    - Enhanced user engagement with interactive animations and smooth scrolling.

--- src/layouts/sections/main/Main.tsx
+++ src/layouts/sections/main/Main.tsx
@@ -2,22 +2,46 @@ import React from 'react';
 import { FlexWrapper } from '../../../components/Flexwrapper';
 import Photo from '../../../assets/images/photo.webp'
 import { Container } from '../../../components/Container';
-import { Link } from '../../../components/link/Link';
 import { S } from './Main_Styles';
+import Typewriter from 'typewriter-effect';
+import Tilt from 'react-parallax-tilt';
+import { scroller } from 'react-scroll';
 
 export const Main: React.FC = () => {
+    const scrollToContact = () => {
+        scroller.scrollTo('contact', {
+            duration: 800,
+            delay: 0,
+            smooth: 'easeInOutQuart'
+        });
+    };
+
     return (
-        <S.Main>
+        <S.Main id='main'>
             <Container>
                 <FlexWrapper flexDirection='column' justify='center' alignItems='flex-start'>
                     <S.Name>Petr Kanapelka</S.Name>
-                    <S.MainSlogan>Frontend developer</S.MainSlogan>
-                    <S.Description>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto tenetur optio aliquam, tempore quasi pariatur voluptatum quaerat. Commodi asperiores amet et, omnis sed ad nemo autem corrupti rerum sapiente dignissimos.</S.Description>
-                    <Link content="Let’s Begin" url='#' />
+                    <S.MainSlogan>
+                        <Typewriter
+                            options={{
+                                strings: ['Frontend developer', 'WEB developer'],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </S.MainSlogan>
+                    <S.Description>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto tenetur optio aliquam, tempore quasi pariatur voluptatum quaerat. Commodi asperiores amet et, omnis sed ad nemo autem corrupti rerum sapiente dignissimos.
+                    </S.Description>
+                    <S.ButtonLink as="button" onClick={scrollToContact}>
+                        Let’s Begin
+                    </S.ButtonLink>
                 </FlexWrapper>
-                <S.ImageWrapper>
-                    <S.Image src={Photo} alt='photo' />
-                </S.ImageWrapper>
+                <Tilt>
+                    <S.ImageWrapper>
+                        <S.Image src={Photo} alt='photo' />
+                    </S.ImageWrapper>
+                </Tilt>
             </Container>
         </S.Main>
     );
