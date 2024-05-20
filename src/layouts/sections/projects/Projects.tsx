e9f54ea311fb46bb122a7aaff392b6f01956eb31
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-05-20 23:12:34 +0300

    feat(projects): add filtering and animation to projects section
    - Added tab menu for filtering projects by type (All, Typescript, Javascript).
    - Implemented state management to handle current filter status.
    - Integrated `framer-motion` for animation when filtering projects.
    - Used `react-awesome-reveal` for fade-in effects on project items.
    - Enhanced user experience with dynamic filtering and smooth animations.
    
    These changes improve the interactivity and visual appeal of the projects section.

--- src/layouts/sections/projects/Projects.tsx
+++ src/layouts/sections/projects/Projects.tsx
@@ -1,20 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Container } from '../../../components/Container';
-import { FlexWrapper } from '../../../components/Flexwrapper';
 import { ProjectData } from './ProjectsData';
 import { Project } from './project/Project';
 import { S } from './Projects_Styles';
+import { Fade } from "react-awesome-reveal";
+import { AnimatePresence, motion } from "framer-motion";
+import { TabMenu, TabsStatusType } from './tabMenu/TabMenu';
+import { FlexWrapper } from '../../../components/Flexwrapper';
+
+const tabsItems: Array<{ title: string, status: TabsStatusType }> = [
+    { title: 'All', status: 'all' },
+    { title: 'Typescript', status: 'ts' },
+    { title: 'Javascript', status: 'js' },
+];
 
 export const Projects: React.FC = () => {
+    const [currentFilterStatus, setCurrentFilterStatus] = useState<TabsStatusType>('all');
+    let filteredWorks = ProjectData;
+
+    if (currentFilterStatus === 'js') {
+        filteredWorks = ProjectData.filter((work) => work.type === 'js');
+    } else if (currentFilterStatus === 'ts') {
+        filteredWorks = ProjectData.filter((work) => work.type === 'ts');
+    }
+
+    function changeFilterStatus(value: TabsStatusType) {
+        setCurrentFilterStatus(value);
+    }
+
     return (
-        <S.Projects>
+        <S.Projects id='projects'>
             <Container>
                 <SectionTitle>Projects</SectionTitle>
+                <TabMenu tabsItems={tabsItems} changeFilterStatus={changeFilterStatus} currentFilterStatus={currentFilterStatus} />
                 <FlexWrapper flexWrap='wrap' justify='space-around' alignItems='center'>
-                    {ProjectData.map((prop, index) => {
-                        return <Project key={index} {...prop} />
-                    })}
+                    <AnimatePresence>
+                        {filteredWorks.map((prop) => {
+                            return (
+                                <motion.div
+                                    key={prop.id}
+                                    layout
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    transition={{ duration: 0.5 }}
+                                >
+                                    <Fade cascade damping={0.1}>
+                                        <Project key={prop.id} {...prop} />
+                                    </Fade>
+                                </motion.div>
+                            );
+                        })}
+                    </AnimatePresence>
                 </FlexWrapper>
             </Container>
         </S.Projects>
